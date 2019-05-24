66a3219cd6669f73c2b187733ac0c84b380e55e9
bobbyjoe91 <bobby.cool00763@gmail.com>
2019-05-24 22:05:55 +0700

    Middleware will be applied

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,15 +17,6 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if ($guard == "translator" && Auth::guard($guard)->check()) {
-            return redirect('/translator');
-        }
-        if ($guard == "personal" && Auth::guard($guard)->check()) {
-            return redirect('/personal');
-        }
-        if ($guard == "company" && Auth::guard($guard)->check()) {
-            return redirect('/company');
-        }
         if (Auth::guard($guard)->check()) {
             return redirect('/home');
         }
